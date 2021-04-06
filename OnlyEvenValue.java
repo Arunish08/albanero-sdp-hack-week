f63729ee59906fef930d30fed86e18823cd3a328
Arunish08 <82014412+Arunish08@users.noreply.github.com>
2021-04-07 03:24:42 +0530

    Add files via upload

---
+++ OnlyEvenValue.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class OnlyEvenValue {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the no. of element in array :  ");
+	int n = s.nextInt();
+	int[] arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		System.out.printf("Enter the %d element of array : ", i);
+		arr[i] = s.nextInt();
+		if(arr[i]<1&&arr[i]>3000)
+		{
+			System.err.println("ERROR : range 1 t0 3000");
+		}
+	}
+		int[] even = new int[n];
+		int temp=0;
+		for(int i=0;i<n;i++)
+		{
+			if(arr[i]%2==0)
+			{
+				even[temp]=arr[i];
+				temp++;
+			}
+		}
+		for(int j=0;j<temp-1;j++)
+		{
+			System.out.printf("%d"+",",even[j]);
+		}
+		//System.out.printf("%d",temp);
+		System.out.printf("%d",even[temp-1]);
+	
+}
+}
