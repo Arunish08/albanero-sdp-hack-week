ac9ccd20cf6982b43924da6246c9f7446d6105f1
Arunish08 <82014412+Arunish08@users.noreply.github.com>
2021-04-06 18:15:54 +0530

    Add files via upload

---
+++ DepthofParantheses.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class DepthofParantheses {
+
+	public static void main(String[] args) {
+		String str;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the string : ");
+		str=s.nextLine();
+		if(str.length()>100) {
+			System.err.println("string length should be less than 100");
+		}
+		if(!str.matches("['[0-9]','+','-','*','/','(',')']+")) {
+			System.err.println(" ERROR ");
+		}
+		int n=0,count=0;
+		int len=str.length();
+		for(int i=0;i<len;i++) {
+			if(str.charAt(i)=='(') {
+				count++;
+				if(count>n) {
+					n=count;
+				}
+			}
+			else if(str.charAt(i)==')') {
+				if(count>0) {
+					count--;
+				}
+				else {
+					System.err.println(" NOT VPS");
+					System.exit(1);
+				}
+			}
+		}
+		if(count!=0) {
+			System.err.println(" NOT VPS");
+			System.exit(1);
+		}
+		
+		System.out.printf("%d",n);
+	}
+
+}
