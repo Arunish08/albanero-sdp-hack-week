d356d4d6a8926a2b33b8b36d1fc60438a07871b1
Arunish08 <82014412+Arunish08@users.noreply.github.com>
2021-04-07 15:18:16 +0530

    Update Day2Question1.java

--- Day2Question1.java
+++ Day2Question1.java
@@ -24,13 +24,25 @@ public class Day2Question1 {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the no. of element in array :  ");
 		int n = s.nextInt();
+		if(n>50||n<1) {
+			System.err.println("No. of element should be integer and less than 50 ");
+			System.exit(1);
+		}
 		String[] arr = new String[n];
 		for (int i = 0; i < n; i++) {
 			System.out.printf("Enter the string in array at %d  : ", i);
 			arr[i] = s.next();
+			if(arr[i].length()>20||arr[i].length()<1) {
+				System.err.println("Word length should integer and less than 20");
+				System.exit(1);
+			}
 		}
 		System.out.println("Enter the pattern string : ");
 		String pattern = s.next();
+		if(pattern.length()>20||pattern.length()<1) {
+			System.err.println("pattern length should integer and less than 20");
+			System.exit(1);
+		}
 
 		String hash = getHash(pattern);
 		String matchWord = "";
